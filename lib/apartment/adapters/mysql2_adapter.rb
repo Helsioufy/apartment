cd2c19bf50bc4aefc5c6e69b069eb39e25d0fe08
Hany Elsioufy <hany.elsioufy@gmail.com>
2016-11-23 14:43:51 +0200

    Switch tenant fix

--- lib/apartment/adapters/mysql2_adapter.rb
+++ lib/apartment/adapters/mysql2_adapter.rb
@@ -2,17 +2,17 @@ require 'apartment/adapters/abstract_adapter'
 
 module Apartment
   module Tenant
-
     def self.mysql2_adapter(config)
-      Apartment.use_schemas ?
-        Adapters::Mysql2SchemaAdapter.new(config) :
+      if Apartment.use_schemas
         Adapters::Mysql2Adapter.new(config)
+      else
+        Adapters::Mysql2ConnectionAdapter.new(config)
+      end
     end
   end
 
   module Adapters
-    class Mysql2Adapter < AbstractAdapter
-
+    class Mysql2ConnectionAdapter < AbstractAdapter
       def initialize(config)
         super
 
@@ -21,14 +21,12 @@ module Apartment
         reset
       end
 
-    protected
-
       def rescue_from
         Mysql2::Error
       end
     end
 
-    class Mysql2SchemaAdapter < AbstractAdapter
+    class Mysql2Adapter < AbstractAdapter
       def initialize(config)
         super
 
@@ -36,37 +34,12 @@ module Apartment
         reset
       end
 
-      #   Reset current tenant to the default_tenant
-      #
-      def reset
-        Apartment.connection.execute "use `#{default_tenant}`"
+      def local_connect(klass, tenant)
+        klass.connection.execute "use `#{environmentify(tenant)}`"
       end
 
-    protected
-
-      #   Connect to new tenant
-      #
-      def connect_to_new(tenant)
-        return reset if tenant.nil?
-
-        Apartment.connection.execute "use `#{environmentify(tenant)}`"
-
-      rescue ActiveRecord::StatementInvalid => exception
-        Apartment::Tenant.reset
-        raise_connect_error!(tenant, exception)
-      end
-
-      def process_excluded_model(model)
-        model.constantize.tap do |klass|
-          # Ensure that if a schema *was* set, we override
-          table_name = klass.table_name.split('.', 2).last
-
-          klass.table_name = "#{default_tenant}.#{table_name}"
-        end
-      end
-
-      def reset_on_connection_exception?
-        true
+      def rescue_from
+        Mysql2::Error
       end
     end
   end
